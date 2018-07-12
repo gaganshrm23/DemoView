eed3461e37e06c957a53c964f61fc026fb25fa9b
Gagan Sharma <gaganshrm23@gmail.com>
2018-07-12 10:08:03 +0530

    Initial Commit

---
+++ DemoView/FirstView.swift
@@ -0,0 +1,21 @@
+//
+//  FirstView.swift
+//  DemoView
+//
+//  Created by Gagan Sharma on 11/07/18.
+//  Copyright © 2018 Gagan Sharma. All rights reserved.
+//
+
+import UIKit
+
+class FirstView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DemoView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoView
+//
+//  Created by Gagan Sharma on 11/07/18.
+//  Copyright © 2018 Gagan Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
